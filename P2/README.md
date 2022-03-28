324d28cff7daaaad638b1fa9746f7f48f42bac7b
adrianjl25 <mrkaisser2000@gmail.com>
2022-03-28 18:04:28 +0200

    Acabado

--- P2/README.md
+++ P2/README.md
@@ -1,1 +1,10 @@
  # Práctica 2
+Funcionamiento de la Calculadora de Malenia: 
+    1. Ingresamos los números que queramos.
+    2. Pulsamos la operación que queramos (solo podremos poner 1 operación)
+    3. Ingresamos los números que queramos.
+    4. Pulsamos = y nos saldrá el resultado.
+Con ese resultamos podemos volver a hacer otra operacioón. También podemos volver a usar ese valor pulsando la tecla "ANS".
+La tecla "DEL" la usamos para borrar el ultimo valor introducido, mientras que con la tecla "AC" borramos todo el espacio de trabajo.
+
+Como mejora, he implementado los números del teclado, es posible trabajar con ellos en la calculadora, tanto las del keypad(los números de la derecha) como las del teclado. Sin embargo, las operaciones tienen que introducirse con el ratón.
--- P2/index.js
+++ P2/index.js
@@ -14,88 +14,166 @@ var button = document.querySelector('.chat-button');
 
 
 var ESTADO = {
-    init: 0,
-    op1: 1,
-    operation: 2,
-    op2: 3
+	init: 0,
+	op1: 1,
+	operation: 2,
+	op2: 3
 };
 
 var estado = ESTADO.init;
 
 for (i = 0; i < numeros.length; i++) {
-    numeros[i].onclick = ev => {
-        digit(ev.target.value);
-        console.log("numero...");
-    };
+	numeros[i].onclick = ev => {
+		digit(ev.target.value);
+		console.log("numero...");
+	};
 }
-for (i = 0; i < operador.length; i++) { 
-    operador[i].onclick = (ev) => {
-        if (estado == ESTADO.op1) {
-            
-            operacion(ev.target.value);
-            console.log("operacion...");
-        }
-    };
+for (i = 0; i < operador.length; i++) {
+	operador[i].onclick = (ev) => {
+		if (estado == ESTADO.op1) {
+
+			operacion(ev.target.value);
+			console.log("operacion...");
+		}
+	};
 }
+
 function digit(num) {
-    if (estado == ESTADO.init) {
-        pantalla.innerHTML = num;
-        estado = ESTADO.op1;
-    }
-    else if (estado == ESTADO.op1) {
-        pantalla.innerHTML += num;
-    }
-    else if(estado == ESTADO.operation) {
-        pantalla.innerHTML += num;
-        estado = ESTADO.op2;
-    }
-    else if(estado == ESTADO.op2) {
-        pantalla.innerHTML += num;
-    }
+	if (estado == ESTADO.init) {
+		pantalla.innerHTML = num;
+		estado = ESTADO.op1;
+	} else if (estado == ESTADO.op1) {
+		pantalla.innerHTML += num;
+	} else if (estado == ESTADO.operation) {
+		pantalla.innerHTML += num;
+		estado = ESTADO.op2;
+	} else if (estado == ESTADO.op2) {
+		pantalla.innerHTML += num;
+	}
 }
 
 function operacion(operador) {
-    if (estado != ESTADO.operation) {
-        pantalla.innerHTML += operador;
-        estado = ESTADO.operation;
-    }
+	if (estado != ESTADO.operation) {
+		pantalla.innerHTML += operador;
+		estado = ESTADO.operation;
+	}
 }
 
 igual.onclick = () => {
-    pantalla.innerHTML = eval(pantalla.innerHTML);
-     answer.value = pantalla.innerHTML;
-     estado = ESTADO.op1;
+	pantalla.innerHTML = eval(pantalla.innerHTML);
+	answer.value = pantalla.innerHTML;
+	estado = ESTADO.op1;
 }
 
 clear.onclick = (ev) => {
-    pantalla.innerHTML = " ";
-    console.log("clear");
-    estado = ESTADO.op1;
+	pantalla.innerHTML = " ";
+	console.log("clear");
+	estado = ESTADO.op1;
 };
 
 borrar.onclick = () => {
-    if (pantalla.innerHTML == "0") {
-      pantalla.innerHTML = "";
-    }else{
-      pantalla.innerHTML = pantalla.innerHTML.slice(0,-1);
-    }
-  };
-   
+	if (pantalla.innerHTML == "0") {
+		pantalla.innerHTML = "";
+	} else {
+		pantalla.innerHTML = pantalla.innerHTML.slice(0, -1);
+	}
+};
+
 answer.onclick = () => {
-      pantalla.innerHTML = answer.value;
-      estado = ESTADO.op1;
-   };
+	pantalla.innerHTML = answer.value;
+	estado = ESTADO.op1;
+};
 
 
-function saludos(){
-    alert('Adelante Sinluz, no temas, adéntrate en el interior del árbor hierático y ofrece tu operaciones a Malenia, espada de Miquella.');
+function saludos() {
+	alert('Adelante Sinluz, no temas, adéntrate en el interior del árbol hierático y ofrece tu operaciones a Malenia, espada de Miquella.');
 }
 
+document.onkeydown = function(ev) {
+	switch (ev.keyCode) {
+		case 48:
+			pantalla.innerHTML += "0";
+			estado = ESTADO.op1;
+			break;
+
+		case 49:
+			pantalla.innerHTML += "1";
+			estado = ESTADO.op1;
+			break;
+		case 50:
+			pantalla.innerHTML += "2";
+			estado = ESTADO.op1;
+			break;
+		case 51:
+			pantalla.innerHTML += "3";
+			estado = ESTADO.op1;
+			break;
+		case 52:
+			pantalla.innerHTML += "4";
+			estado = ESTADO.op1;
+			break;
+		case 53:
+			pantalla.innerHTML += "5";
+			estado = ESTADO.op1;
+			break;
+		case 54:
+			pantalla.innerHTML += "6";
+			estado = ESTADO.op1;
+			break;
+		case 55:
+			pantalla.innerHTML += "7";
+			estado = ESTADO.op1;
+			break;
+		case 56:
+			pantalla.innerHTML += "8";
+			estado = ESTADO.op1;
+			break;
+		case 57:
+			pantalla.innerHTML += "9";
+			estado = ESTADO.op1;
+			break;
+		case 96:
+			pantalla.innerHTML += "0";
+			estado = ESTADO.op1;
+			break;
+		case 97:
+			pantalla.innerHTML += "1";
+			estado = ESTADO.op1;
+			break;
+		case 98:
+			pantalla.innerHTML += "2";
+			estado = ESTADO.op1;
+			break;
+		case 99:
+			pantalla.innerHTML += "3";
+			estado = ESTADO.op1;
+			break;
+		case 100:
+			pantalla.innerHTML += "4";
+			estado = ESTADO.op1;
+			break;
+		case 101:
+			pantalla.innerHTML += "5";
+			estado = ESTADO.op1;
+			break;
+		case 102:
+			pantalla.innerHTML += "6";
+			estado = ESTADO.op1;
+			break;
+		case 103:
+			pantalla.innerHTML += "7";
+			estado = ESTADO.op1;
+			break;
+		case 104:
+			pantalla.innerHTML += "8";
+			estado = ESTADO.op1;
+			break;
+		case 105:
+			pantalla.innerHTML += "9";
+			estado = ESTADO.op1;
+			break;
+	}
 
 
 
-
-
-
-
-
+}
