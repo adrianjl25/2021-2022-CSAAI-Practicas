85bd91d24113d4525bc07730f2f4ce78ff3de6f2
adrianjl25 <mrkaisser2000@gmail.com>
2022-05-10 10:53:40 +0200

    Terminado

--- P5/index.js
+++ P5/index.js
@@ -2,15 +2,24 @@
 let directo = document.getElementById("directo");
 const video1 = document.getElementById("video1");
 const btn_video1 = document.getElementById("btn_video1");
+const video2 = document.getElementById("video2");
+const btn_video2 = document.getElementById("btn_video2");
+const video3 = document.getElementById("video3");
+const btn_video3 = document.getElementById("btn_video3");
 const btn_test = document.getElementById("btn_test");
 const btn_src_on = document.getElementById("btn_src_on");
+const btn_src_off = document.getElementById("btn_src_off");
 
 
 //-- Establecer las dimensiones de los vídeos
 directo.width=720;
 directo.height=640;
-video1.width=200;  
-video1.height=100;
+video1.width=250;  
+video1.height=150;
+video2.width=250;  
+video2.height=150;
+video3.width=250;  
+video3.height=150;
 
 //-- Imagen de Test usada
 const TEST_IMAGE_URL = "test.png";
@@ -19,6 +28,8 @@ const TEST_IMAGE_URL = "test.png";
 //-- ha arrancado
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
+video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
@@ -32,6 +43,23 @@ btn_src_on.onclick = () => {
   //-- Y en silencio...
   video1.muted;
 
+  //-- Establecer la fuente de la cámara 2
+  video2.src="zelda.mp4";
+  //-- Reprodeucimos un vídeo, desde el comienzo
+  video2.currentTime = 0;
+  video2.play();
+
+  //-- Y en silencio...
+  video2.muted;
+  
+  //-- Establecer la fuente de la cámara 3
+  video3.src="zelda3.mp4";
+  //-- Reprodeucimos un vídeo, desde el comienzo
+  video3.currentTime = 0;
+  video3.play();
+
+  //-- Y en silencio...
+  video3.muted;
   //-- En la emisión en directo ponemos la imagen de prueba
   directo.poster = TEST_IMAGE_URL;
 };
@@ -49,3 +77,29 @@ btn_video1.onclick = () => {
     directo.play();
     directo.poster=null;
 };
+
+//-- Botón de Selección de la cámara 2
+btn_video2.onclick = () => {
+    directo.src = video2.src;
+    directo.currentTime = video2.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+//-- Botón de Selección de la cámara 3
+btn_video3.onclick = () => {
+    directo.src = video3.src;
+    directo.currentTime = video3.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+btn_src_off.onclick = () => {
+    directo.poster = TEST_IMAGE_URL;
+    video1.poster = TEST_IMAGE_URL;
+    video2.poster = TEST_IMAGE_URL;
+    video3.poster = TEST_IMAGE_URL;
+
+    directo.src = null;
+    video1.src = null;
+    video2.src = null;
+    video3.src = null;
+}
